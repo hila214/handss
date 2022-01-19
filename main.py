1ec37380df979fd27f665f6b42aed1b10679e994
emailmeydrive <emailme.ydrive@gmail.com>
2022-01-19 14:39:41 +0200

    new2

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from pickle import FALSE
 from turtle import circle
 import pygame
 import time
 
 # screen size 
 WINDOW_W = 1000
-WINDOW_H = 600
+WINDOW_H = 666
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 
 BK_COLOR = (255,204,204)
@@ -12,17 +13,32 @@ BK_COLOR = (255,204,204)
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("Hila First Game")
-bk_image = pygame.image.load("bk_img.jpg")
+bk_image = pygame.image.load("img1.jpg")
+ship_image= pygame.image.load("ship3.png.png")
+ship_image= pygame.transform.scale(ship_image, (50,80))
 
 clock= pygame.time.Clock()
 circle_x = 10
 circle_y= WINDOW_H/2
 x_step = 10
-
 play = True
+ship_x= WINDOW_W/2
+ship_y= WINDOW_H - 80
+
 while play:
   # screen.fill(BK_COLOR)
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      play = False
+    elif event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        ship_x= -10
+      if event.key == pygame.K_RIGHT:
+        ship_x = +10
+
+ # screen.fill(BK_COLOR)
   screen.blit(bk_image,(0,0))
+  screen.blit(ship_image,(ship_x ,ship_y))
   pygame.draw.circle(screen,(120,89,255),(circle_x , circle_y  ),10)
 
 
